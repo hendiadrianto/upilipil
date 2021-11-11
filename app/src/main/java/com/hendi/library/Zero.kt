61b9ba7367e026e390645c7975463a8e244bce3d
Hendi <hendi.adrianto@gmail.com>
2021-11-11 21:29:28 +0700

    Initial commit

---
+++ app/src/main/java/com/hendi/library/Zero.kt
@@ -0,0 +1,17 @@
+package com.hendi.library
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class Zero : AppCompatActivity() {
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
+
+        var a = 10
+
+    }
+
+}
---
+++ upilipil/src/main/java/com/hendi/upilipil/zero.kt
@@ -0,0 +1,25 @@
+package com.hendi.upilipil
+
+fun Int?.orZero(): Int {
+    return this ?: 0
+}
+
+fun Double?.orZero(): Double {
+    return this ?: 0.0
+}
+
+fun Float?.orZero(): Float {
+    return this ?: 0.0f
+}
+
+fun Long?.orZero(): Long {
+    return this ?: 0L
+}
+
+fun Byte?.orZero(): Byte {
+    return this ?: 0
+}
+
+fun Short?.orZero(): Short {
+    return this ?: 0
+}
