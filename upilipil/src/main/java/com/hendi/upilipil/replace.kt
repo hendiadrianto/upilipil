dd37cb18152158609d515af32afbedca8f98286c
Hendi <hendi.adrianto@gmail.com>
2022-08-12 17:52:17 +0700

    Update replace.kt

--- upilipil/src/main/java/com/hendi/upilipil/replace.kt
+++ upilipil/src/main/java/com/hendi/upilipil/replace.kt
@@ -1,20 +1,22 @@
 package com.hendi.upilipil
 
-import java.text.DecimalFormat
+import java.text.NumberFormat
 
 fun String.resetFormat(): String {
     val re = Regex("[^A-Za-z0-9 ]")
     return re.replace(this, "")
 }
 
-fun String?.decimalFormat(currency : String = ""): String {
-    val decimalFormat = DecimalFormat("#,###")
-    if (currency.isNotEmpty()){
-        return "$currency ${decimalFormat.format(this?.toInt().orZero())}"
+fun String?.decimalFormat(currency: String = ""): String {
+    val number = this?.split(".")?.get(0)?.toLong().orZero()
+    val numberFormat: NumberFormat = NumberFormat.getNumberInstance()
+
+    if (currency.isNotEmpty()) {
+        return "$currency ${numberFormat.format(number)}"
     }
-    return decimalFormat.format(this?.toInt().orZero())
+    return numberFormat.format(number)
 }
 
-fun String?.replacePointCommaWithEmptyString() : String {
-    return this?.replace(".","")?.replace(",","") ?: ""
+fun String?.replacePointCommaWithEmptyString(): String {
+    return this?.replace(".", "")?.replace(",", "") ?: ""
 }
